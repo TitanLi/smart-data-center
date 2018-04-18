01ea0cc978a83b23d7cbfa66e13fc64066a99ab4
TitanLi <Lisheng0706@gmail.com>
2018-04-18 11:38:15 +0800

    Add pie UI

---
+++ public/script/highcharts/pie-3D.js
@@ -0,0 +1,47 @@
+$(function () {
+    $('#pie').highcharts({
+        chart: {
+            backgroundColor: [43, 40, 40, 0.71],
+            type: 'pie',
+            options3d: {
+                enabled: true,
+                alpha: 45,
+                beta: 0
+            }
+        },
+        title: {
+            text: '耗電比'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                depth: 35,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: '浏览器占比',
+            data: [
+                ['Firefox',   45.0],
+                ['IE',       26.8],
+                {
+                    name: 'Chrome',
+                    y: 12.8,
+                    sliced: true,
+                    selected: true
+                }
+            ]
+        }],
+        credits: {
+          enabled: false
+        }
+    });
+});
