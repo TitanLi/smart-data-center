1ea089b6bbeb0e291b8487dd12356084c802acc6
TitanLi <Lisheng0706@gmail.com>
2018-04-18 11:38:15 +0800

    Add pie UI

--- public/script/highcharts/pie.js
+++ public/script/highcharts/pie.js
@@ -1,50 +1,71 @@
-$(function () {
-    $('#pie').highcharts({
-        chart: {
-            backgroundColor: [43, 40, 40, 0.71],
-            type: 'pie',
-            options3d: {
+// Make monochrome colors
+var pieColors = (function () {
+    var colors = [],
+        base = Highcharts.getOptions().colors[0],
+        i;
+
+    for (i = 0; i < 10; i += 1) {
+        // Start out with a darkened base color (negative brighten), and end
+        // up with a much brighter color
+        colors.push(Highcharts.Color(base).brighten((i - 3) / 7).get());
+    }
+    return colors;
+}());
+
+// Build the chart
+Highcharts.chart('container', {
+    chart: {
+        backgroundColor: [43, 40, 40, 0.71],
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        style: {
+          color:"#ffffff"
+        },
+        verticalAlign: 'top',
+        y:40,
+        text: '消耗功率比例'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            colors: pieColors,
+            dataLabels: {
                 enabled: true,
-                alpha: 45,
-                beta: 0
-            }
-        },
-        title: {
-            text: '耗電比'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                depth: 35,
-                dataLabels: {
-                    enabled: true,
-                    format: '{point.name}'
+                format: '<b>{point.name}</b><br>{point.percentage:.1f} %',
+                distance: -46,
+                filter: {
+                    property: 'percentage',
+                    operator: '>',
+                    value: 4
+                },
+                style: {
+                    fontSize: '16px'
                 }
             }
-        },
-        series: [{
-            type: 'pie',
-            name: '浏览器占比',
-            data: [
-                ['Firefox',   45.0],
-                ['IE',       26.8],
-                {
-                    name: 'Chrome',
-                    y: 12.8,
-                    sliced: true,
-                    selected: true
-                },
-                ['Safari',    8.5],
-                ['Opera',     6.2],
-                ['Others',   0.7]
-            ]
-        }],
-        credits: {
-          enabled: false
         }
-    });
+    },
+    series: [{
+        name: 'Share',
+        data: [
+            { name: '冷氣', y: 20 },
+            { name: 'UPS1', y: 40 },
+            { name: 'UPS2', y: 40 }
+        ]
+    }],
+    navigation:{
+      buttonOptions: {
+        enabled:false
+      }
+    },
+    credits: {
+      enabled: false
+    }
 });
