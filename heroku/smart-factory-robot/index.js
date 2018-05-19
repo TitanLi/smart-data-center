beb48534ac6ad2ceab7c7d20224c2d247dc38418
TitanLi <Lisheng0706@gmail.com>
2018-05-19 15:51:14 +0800

    Add carousel template

--- heroku/smart-factory-robot/index.js
+++ heroku/smart-factory-robot/index.js
@@ -20,6 +20,7 @@ const router = Router();
 const linebot = new Linebot(process.env.channelSecret,process.env.lineBotToken);
 
 var test = "Hello Koa";
+// var userId = '';
 
 app.use(json());
 app.use(logger());
@@ -51,6 +52,7 @@ router.post('/webhooks', async (ctx, next) => {
   let mLabData = "";
   let useMsg = "";
   let events = linebot.requestHandle(ctx);
+
   if (events) {
       test = JSON.stringify(events);
       let messageText = events.messageText;
@@ -108,12 +110,86 @@ router.post('/webhooks', async (ctx, next) => {
     }
 });
 
-router.post("/push",async function(ctx,next){
+router.post('/push',async function(ctx,next){
   let requestData = ctx.request.body;
   let imacGroupID = process.env.imacGroupID;
   let messageText = '昨日冷氣消耗度數：'+requestData.powerMeterPower+'度\n'+'昨日ups_A消耗度數：'+requestData.upsPower_A+'度\n'+'昨日ups_B消耗度數：'+requestData.upsPower_B+'度';
   // 發送給imac group
   let data = await linebot.sendText(imacGroupID,messageText);
+  ctx.status = 200;
+  ctx.body = data;
+});
+
+router.get('/test',async function(ctx,next){
+  let userId = 'U6ac46864e9646bd5bd954471f0060194';
+  let altText = '遠端控制';
+  let thumbnailImageUrl = ['https://i.imgur.com/O8lp0mk.png','https://i.imgur.com/icAeax3.png','https://i.imgur.com/0GJsShU.jpg'];
+  let imageBackgroundColor = ['#FFEE99','#FFEE99','#FFEE99'];
+  let title = ['排風風扇','進風風扇','加溼器'];
+  let text = ['狀態','狀態','狀態'];
+  let defaultAction = [
+    {
+      'type': 'message',
+      'label': '排風風扇',
+      'text':'排風風扇'
+    },
+    {
+      'type': 'message',
+      'label': '進風風扇',
+      'text':'進風風扇'
+    },
+    {
+      'type': 'message',
+      'label': '加溼器',
+      'text':'加溼器'
+    }
+  ];
+  let actions = [
+    [
+      {
+        "type": "postback",
+        "label": "開啟",
+        "data":"action=buy&itemid=111"
+      },
+      {
+        "type": "postback",
+        "label": "關閉",
+        "data":"action=buy&itemid=111"
+      }
+    ],
+    [
+      {
+        "type": "postback",
+        "label": "開啟",
+        "data":"action=buy&itemid=111"
+      },
+      {
+        "type": "postback",
+        "label": "關閉",
+        "data":"action=buy&itemid=111"
+      }
+    ],
+    [
+      {
+        "type": "postback",
+        "label": "開啟",
+        "data":"action=buy&itemid=111"
+      },
+      {
+        "type": "postback",
+        "label": "關閉",
+        "data":"action=buy&itemid=111"
+      }
+    ]
+  ];
+  let data = await linebot.carouselTemplate(userId,
+                                            altText,
+                                            thumbnailImageUrl,
+                                            imageBackgroundColor,
+                                            title,
+                                            text,
+                                            defaultAction,
+                                            actions);
   ctx.status = 200;
   ctx.body = data;
 });
