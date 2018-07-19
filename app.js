d49e1ad77f4979bfc38140fb65e4c0dd28a5762c
TitanLi <Lisheng0706@gmail.com>
2018-07-19 10:27:29 +0800

    Debug request-promise error Add catch error function Error message: Error: getaddrinfo ENOTFOUND smart-factory-robot.herokuapp.com smart-factory-robot.herokuapp.com:443

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const mongodb = new mongoDB(io);
 const mqttClient = mqtt.connect(process.env.MQTT);
 
 //ET7044 status
-var et7044Status,D0,D1,D2;
+var et7044Status, D0, D1, D2;
 //power-meter data
 // var humidity,temperature,powerMeterCurrent;
 //UPS1 data
@@ -32,80 +32,80 @@ var et7044Status,D0,D1,D2;
 //UPS2 data
 // var inputVolt_B,inputFreq_B,outputVolt_B,outputFreq_B,outputAmp_B,outputWatt_B,systemMode_B,outputPercent_B,batteryHealth_B,batteryCharge_Mode_B,batteryTemp_B,batteryRemain_B;
 
-mqttClient.on('connect',() => {
+mqttClient.on('connect', () => {
   mqttClient.subscribe('UPS_Monitor');
   mqttClient.subscribe('current');
   mqttClient.subscribe('ET7044/DOstatus');
 });
 
-mqttClient.on('message',(topic,message) => {
+mqttClient.on('message', (topic, message) => {
   // console.log(topic,JSON.parse(message));
   switch (topic) {
     //power-meter MQTT input data
     case 'current':
       let powerMeterMqttData = JSON.parse(message);
-      io.emit('humidity',powerMeterMqttData.Humidity);
-      io.emit('temperature',powerMeterMqttData.Temperature);
-      io.emit('current',powerMeterMqttData.currents);
+      io.emit('humidity', powerMeterMqttData.Humidity);
+      io.emit('temperature', powerMeterMqttData.Temperature);
+      io.emit('current', powerMeterMqttData.currents);
       break;
     //UPS MQTT input data
     case 'UPS_Monitor':
       let upsMqttData = JSON.parse(message);
-      io.emit('inputVolt_A',upsMqttData.input_A.inputVolt_A);
-      io.emit('inputFreq_A',upsMqttData.input_A.inputFreq_A);
-      io.emit('outputVolt_A',upsMqttData.output_A.outputVolt_A);
-      io.emit('outputFreq_A',upsMqttData.output_A.outputFreq_A);
-      io.emit('outputAmp_A',upsMqttData.output_A.outputAmp_A);
-      io.emit('outputWatt_A',upsMqttData.output_A.outputWatt_A);
-      io.emit('systemMode_A',upsMqttData.output_A.systemMode_A);
-      io.emit('outputPercent_A',upsMqttData.output_A.outputPercent_A);
-      io.emit('batteryHealth_A',upsMqttData.battery_A.status.batteryHealth_A);
-      io.emit('batteryCharge_Mode_A',upsMqttData.battery_A.status.batteryCharge_Mode_A.split("(")[0]);
-      io.emit('batteryTemp_A',upsMqttData.battery_A.status.batteryTemp_A);
-      io.emit('batteryRemain_A',upsMqttData.battery_A.status.batteryRemain_Min_A == "None By Charging (充電中)" || upsMqttData.battery_A.status.batteryRemain_Sec_A == "None By Charging (充電中)" ?
-                                "充電中" : upsMqttData.battery_A.status.batteryRemain_Min_A + ":" + upsMqttData.battery_A.status.batteryRemain_Sec_A);
-      io.emit('batteryRemain_Percent_A',upsMqttData.battery_A.status.batteryRemain_Percent_A);
-      io.emit('inputVolt_B',upsMqttData.input_B.inputVolt_B);
-      io.emit('inputFreq_B',upsMqttData.input_B.inputFreq_B);
-      io.emit('outputVolt_B',upsMqttData.output_B.outputVolt_B);
-      io.emit('outputFreq_B',upsMqttData.output_B.outputFreq_B);
-      io.emit('outputAmp_B',upsMqttData.output_B.outputAmp_B);
-      io.emit('outputWatt_B',upsMqttData.output_B.outputWatt_B);
-      io.emit('systemMode_B',upsMqttData.output_B.systemMode_B);
-      io.emit('outputPercent_B',upsMqttData.output_B.outputPercent_B);
-      io.emit('batteryHealth_B',upsMqttData.battery_B.status.batteryHealth_B);
-      io.emit('batteryCharge_Mode_B',upsMqttData.battery_B.status.batteryCharge_Mode_B.split("(")[0]);
-      io.emit('batteryTemp_B',upsMqttData.battery_B.status.batteryTemp_B);
-      io.emit('batteryRemain_B',upsMqttData.battery_B.status.batteryRemain_Min_B == "None By Charging (充電中)" || upsMqttData.battery_B.status.batteryRemain_Sec_B == "None By Charging (充電中)" ?
-                                "充電中" : upsMqttData.battery_B.status.batteryRemain_Min_B + ":" + upsMqttData.battery_B.status.batteryRemain_Sec_B);
-      io.emit('batteryRemain_Percent_B',upsMqttData.battery_B.status.batteryRemain_Percent_B);
+      io.emit('inputVolt_A', upsMqttData.input_A.inputVolt_A);
+      io.emit('inputFreq_A', upsMqttData.input_A.inputFreq_A);
+      io.emit('outputVolt_A', upsMqttData.output_A.outputVolt_A);
+      io.emit('outputFreq_A', upsMqttData.output_A.outputFreq_A);
+      io.emit('outputAmp_A', upsMqttData.output_A.outputAmp_A);
+      io.emit('outputWatt_A', upsMqttData.output_A.outputWatt_A);
+      io.emit('systemMode_A', upsMqttData.output_A.systemMode_A);
+      io.emit('outputPercent_A', upsMqttData.output_A.outputPercent_A);
+      io.emit('batteryHealth_A', upsMqttData.battery_A.status.batteryHealth_A);
+      io.emit('batteryCharge_Mode_A', upsMqttData.battery_A.status.batteryCharge_Mode_A.split("(")[0]);
+      io.emit('batteryTemp_A', upsMqttData.battery_A.status.batteryTemp_A);
+      io.emit('batteryRemain_A', upsMqttData.battery_A.status.batteryRemain_Min_A == "None By Charging (充電中)" || upsMqttData.battery_A.status.batteryRemain_Sec_A == "None By Charging (充電中)" ?
+        "充電中" : upsMqttData.battery_A.status.batteryRemain_Min_A + ":" + upsMqttData.battery_A.status.batteryRemain_Sec_A);
+      io.emit('batteryRemain_Percent_A', upsMqttData.battery_A.status.batteryRemain_Percent_A);
+      io.emit('inputVolt_B', upsMqttData.input_B.inputVolt_B);
+      io.emit('inputFreq_B', upsMqttData.input_B.inputFreq_B);
+      io.emit('outputVolt_B', upsMqttData.output_B.outputVolt_B);
+      io.emit('outputFreq_B', upsMqttData.output_B.outputFreq_B);
+      io.emit('outputAmp_B', upsMqttData.output_B.outputAmp_B);
+      io.emit('outputWatt_B', upsMqttData.output_B.outputWatt_B);
+      io.emit('systemMode_B', upsMqttData.output_B.systemMode_B);
+      io.emit('outputPercent_B', upsMqttData.output_B.outputPercent_B);
+      io.emit('batteryHealth_B', upsMqttData.battery_B.status.batteryHealth_B);
+      io.emit('batteryCharge_Mode_B', upsMqttData.battery_B.status.batteryCharge_Mode_B.split("(")[0]);
+      io.emit('batteryTemp_B', upsMqttData.battery_B.status.batteryTemp_B);
+      io.emit('batteryRemain_B', upsMqttData.battery_B.status.batteryRemain_Min_B == "None By Charging (充電中)" || upsMqttData.battery_B.status.batteryRemain_Sec_B == "None By Charging (充電中)" ?
+        "充電中" : upsMqttData.battery_B.status.batteryRemain_Min_B + ":" + upsMqttData.battery_B.status.batteryRemain_Sec_B);
+      io.emit('batteryRemain_Percent_B', upsMqttData.battery_B.status.batteryRemain_Percent_B);
       break;
     case 'ET7044/DOstatus':
       et7044Status = JSON.parse(message);
-      io.emit('D0',et7044Status[0]);
-      io.emit('D1',et7044Status[1]);
-      io.emit('D2',et7044Status[2]);
+      io.emit('D0', et7044Status[0]);
+      io.emit('D1', et7044Status[1]);
+      io.emit('D2', et7044Status[2]);
       //ET7044 status translate
-      function getStatus(status){
-        if(status){
+      function getStatus(status) {
+        if (status) {
           return '開啟';
-        }else{
+        } else {
           return '關閉';
         }
       }
       //D0狀態被改變時驅動
-      if(D0 != et7044Status[0]){
-        io.emit('ET7044',`${new Date().toLocaleString('zh-tw')} 進風風扇：${getStatus(et7044Status[0])}\n`);
+      if (D0 != et7044Status[0]) {
+        io.emit('ET7044', `${new Date().toLocaleString('zh-tw')} 進風風扇：${getStatus(et7044Status[0])}\n`);
       }
 
       //D1狀態被改變時驅動
-      if(D1 != et7044Status[1]){
-        io.emit('ET7044',`${new Date().toLocaleString('zh-tw')} 加溼器：${getStatus(et7044Status[1])}\n`);
+      if (D1 != et7044Status[1]) {
+        io.emit('ET7044', `${new Date().toLocaleString('zh-tw')} 加溼器：${getStatus(et7044Status[1])}\n`);
       }
 
       //D2狀態被改變時驅動
-      if(D2 != et7044Status[2]){
-        io.emit('ET7044',`${new Date().toLocaleString('zh-tw')} 排風風扇：${getStatus(et7044Status[2])}\n`);
+      if (D2 != et7044Status[2]) {
+        io.emit('ET7044', `${new Date().toLocaleString('zh-tw')} 排風風扇：${getStatus(et7044Status[2])}\n`);
       }
 
       //更新ET7044 D0~D3狀態
@@ -124,14 +124,14 @@ mqttClient.on('message',(topic,message) => {
 //更新圓餅圖
 setInterval(() => {
   mongodb.aggregateAvgPieData();
-},600000);
+}, 600000);
 
 // setInterval(() => {
 //   mongodb.aggregateYesterdayAvgPowerRobot();
 // },10000);
 
 setInterval(() => {
-  if(new Date().toLocaleString('zh-tw').split(' ')[1] == "08:01:00"){
+  if (new Date().toLocaleString('zh-tw').split(' ')[1] == "08:01:00") {
     let push = async () => {
       let data = await mongodb.aggregateYesterdayAvgPowerRobot();
       let options = {
@@ -141,25 +141,31 @@ setInterval(() => {
           'Content-Type': 'application/json'
         },
         body: {
-          powerMeterPower : data.powerMeterPower,
-          upsPower_A : data.upsPower_A,
-          upsPower_B : data.upsPower_B
+          powerMeterPower: data.powerMeterPower,
+          upsPower_A: data.upsPower_A,
+          upsPower_B: data.upsPower_B
         },
         json: true
       }
-      await request(options);
+      await request(options).then(function (parsedBody) {
+        console.log(parsedBody);
+        console.log('aggregateYesterdayAvgPowerRobot post success')
+      }).catch(function (err) {
+          console.error(err);
+          push();
+      });
     }
     push();
   }
-},1000);
+}, 1000);
 
 app.use(json());
 app.use(logger());
 app.use(bodyParser());
-app.use(serve(__dirname+'/public/img'));
-app.use(serve(__dirname+'/public/css'));
-app.use(serve(__dirname+'/public/script'));
-app.use(serve(__dirname+'/lib'));
+app.use(serve(__dirname + '/public/img'));
+app.use(serve(__dirname + '/public/css'));
+app.use(serve(__dirname + '/public/script'));
+app.use(serve(__dirname + '/lib'));
 app.use(router.routes());
 
 app.context.render = co.wrap(render({
@@ -169,48 +175,48 @@ app.context.render = co.wrap(render({
   ext: 'html',
 }));
 
-router.get('/',index);
-router.get('/test',test);
-router.get('/pie',pie);
-router.get('/temperature',temperature);
-router.post('/ET7044',et7044);
+router.get('/', index);
+router.get('/test', test);
+router.get('/pie', pie);
+router.get('/temperature', temperature);
+router.post('/ET7044', et7044);
 
-async function index(ctx){
+async function index(ctx) {
   let piePercent = await mongodb.aggregateAvgPieData();
-  ctx.body = await ctx.render('smart',{
-                                       "powerMeterPower":piePercent[0].y,
-                                       "upsPower_A":piePercent[1].y,
-                                       "upsPower_B":piePercent[2].y,
-                                       "socket":process.env.SOCKET
-                                     });
+  ctx.body = await ctx.render('smart', {
+    "powerMeterPower": piePercent[0].y,
+    "upsPower_A": piePercent[1].y,
+    "upsPower_B": piePercent[2].y,
+    "socket": process.env.SOCKET
+  });
 }
 
-async function test(ctx){
+async function test(ctx) {
   ctx.body = await ctx.render('test');
 }
 
-async function pie(ctx){
+async function pie(ctx) {
   ctx.body = await ctx.render('pie');
 }
 
-async function temperature(ctx){
+async function temperature(ctx) {
   ctx.body = await ctx.render('temperature');
 }
 
-async function et7044(ctx){
+async function et7044(ctx) {
   let et7044 = ctx.request.body.data;
   switch (et7044) {
     case 'D0':
       et7044Status[0] = !et7044Status[0];
-      mqttClient.publish('ET7044/write',JSON.stringify(et7044Status));
+      mqttClient.publish('ET7044/write', JSON.stringify(et7044Status));
       break;
     case 'D1':
       et7044Status[1] = !et7044Status[1];
-      mqttClient.publish('ET7044/write',JSON.stringify(et7044Status));
+      mqttClient.publish('ET7044/write', JSON.stringify(et7044Status));
       break;
     case 'D2':
       et7044Status[2] = !et7044Status[2];
-      mqttClient.publish('ET7044/write',JSON.stringify(et7044Status));
+      mqttClient.publish('ET7044/write', JSON.stringify(et7044Status));
       break;
     default:
       console.log('pass');
@@ -220,7 +226,7 @@ async function et7044(ctx){
   ctx.body = et7044;
 }
 
-server.listen(process.env.PORT, function() {
-    let port = server.address().port;
-    console.log("App now running on port", port);
-  });
+server.listen(process.env.PORT, function () {
+  let port = server.address().port;
+  console.log("App now running on port", port);
+});
