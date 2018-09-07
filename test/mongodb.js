ae03ad23b9b7dd47b8c7ea487c6d0bf46eba62e0
TitanLi <Lisheng0706@gmail.com>
2018-09-07 23:06:49 +0800

    Debug yesterdayStart and yesterdayEnd value in aggregateYesterdayAvgPowerRobot function

--- test/mongodb.js
+++ test/mongodb.js
@@ -1,27 +1,40 @@
 const MongoClient = require('mongodb').MongoClient;
+const co = require('co');
 
-MongoClient.connect("mongodb://localhost:27017/", (err, client) => {
-    db = client.db("smart-factory");
+MongoClient.connect("mongodb://10.20.0.19:27017/", (err, client) => {
+  db = client.db("smart-factory");
 
-    // 2018-04-20T09:00:00.000Z 2018-04-20T09:59:59.000Z
-    //test
+  co(function* () {
+    var date = new Date();
+    let yesterday = new Date(date.setDate(date.getDate() - 1));
+    let yesterdayStart = new Date((new Date(yesterday).getMonth() + 1) + " " + (new Date(yesterday).getDate()) + ", " + new Date(yesterday).getUTCFullYear() + " 00:00:00");
+    let yesterdayEnd = new Date((new Date(yesterday).getMonth() + 1) + " " + (new Date(yesterday).getDate()) + ", " + new Date(yesterday).getUTCFullYear() + " 23:59:59");
+    let yesterdayPowerMeterPower;
+    console.log(yesterday, yesterdayStart, yesterdayEnd);
 
-    // { $group: {
-    //             _id: {
-    //               $hour: {
-    //                 date: "$time",
-    //                 timezone: "Asia/Taipei"
-    //               }
-    //             },
-    //             upsPower_A:{$avg:"$power"}
-    //           }
-    // }
-    var collection = db.collection('upsPower_A');
-    collection.aggregate(
-          [ { $match : {"time":{"$gte":new Date("2018-04-20T09:00:00.000Z"),"$lte":new Date("2018-04-20T09:59:59.000Z")}}},
-
-          ],(err,data) => {
+    yield function (done) {
+      var collectionPowerMeterPower = db.collection('powerMeterPower');
+      collectionPowerMeterPower.aggregate(
+        [{ $match: { "time": { "$gte": yesterdayStart, "$lte": yesterdayEnd } } },
+        {
+          $group: {
+            _id: null,
+            powerMeterPower: { $avg: "$power" }
+          }
+        }
+        ], (err, data) => {
+          if (err) {
+            console.log(err);
+            done();
+          } else {
             console.log(data);
-          });
-    console.log(123);
+            if (data.length != 0) {
+              yesterdayPowerMeterPower = data[0].powerMeterPower;
+            }
+            done();
+          }
+        }
+      )
+    };
+  });
 });
