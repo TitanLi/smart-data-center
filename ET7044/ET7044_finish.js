cf74caacea7fc57836c1fc1c7e120114302ce75b
TitanLi <Lisheng0706@gmail.com>
2018-09-16 12:45:02 +0800

    Add ET7044

---
+++ ET7044/ET7044_finish.js
@@ -0,0 +1,82 @@
+// create an empty modbus client
+var mqtt = require('mqtt');
+var ModbusRTU = require("modbus-serial");
+var client = new ModbusRTU();
+var timeoutRunRef = null;
+var timeoutConnectRef = null;
+var networkErrors = ["ESOCKETTIMEDOUT", "ETIMEDOUT", "ECONNRESET", "ECONNREFUSED"];
+var writeData = [false, false, false, false, false, false, false, false];
+var config = require("./config.json");
+
+function checkError(e) {
+    if (e.errno && networkErrors.includes(e.errno)) {
+        console.log("we have to reconnect");
+        // close port
+        client.close();
+        // re open client
+        client = new ModbusRTU();
+        timeoutConnectRef = setTimeout(connect, 1000);
+    }
+}
+function connect() {
+    // clear pending timeouts
+    clearTimeout(timeoutConnectRef);
+    // if client already open, just run
+    if (client.isOpen()) {
+        run();
+    }
+
+    client.connectTCP(config.ET7044, { port: 502 })
+        .then(setClient)
+        .then(function () {
+            console.log("Connected");
+        })
+        .catch(function (e) {
+            console.log(e.message);
+        });
+}
+function setClient() {
+    // set the client's unit id
+    // set a timout for requests default is null (no timeout)
+    client.setID(1);
+    client.setTimeout(3000);
+    // run program
+    run();
+}
+var DOstatus;
+function run() {
+    // clear pending timeouts
+    clearTimeout(timeoutRunRef);
+    client.writeCoils(0, writeData);
+    client.readCoils(0, 8)
+        .then(function (d) {
+            //DOstatus = d.data.toString();
+            DOstatus = JSON.stringify(d.data);
+            console.log(DOstatus);
+            console.log("Receive:", d.data);
+            mqttClient.publish('ET7044/DOstatus', DOstatus);
+        })
+        .then(function () {
+            timeoutRunRef = setTimeout(run, 5000);
+        })
+        .catch(function (e) {
+            checkError(e);
+            console.log(e.message);
+        });
+
+}
+// connect and start logging
+connect();
+// Mqtt connecting and pub
+var mqttClient = mqtt.connect(config.MQTT);
+mqttClient.on('connect', function () {
+    console.log('connect to MQTT server');
+    mqttClient.subscribe('ET7044/write');
+});
+
+mqttClient.on('message', function (topic, message) {
+    // message is Buffer
+    writeData = JSON.parse(message);
+    console.log(writeData);
+
+});
