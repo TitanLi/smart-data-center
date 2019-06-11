395384637a862bcb40d2d7abaa612d6a4c4aa860
TitanLi <lisheng0706@gmail.com>
2019-06-11 12:45:05 +0800

    Update text

--- heroku/smart-factory-robot/index.js
+++ heroku/smart-factory-robot/index.js
@@ -211,7 +211,7 @@ app
     .use(linebot.middleware())
     .use(router.routes());
 
-// //因為 koa 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+// 因為 koa 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
