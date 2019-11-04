159ffe133a2c5e0ec4520ce4e2195b705a497519
TitanLi <lisheng0706@gmail.com>
2019-11-04 09:40:47 +0800

    Debug time

--- service/service-list.js
+++ service/service-list.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 setInterval(() => {
     // if (new Date().toLocaleString('zh-tw').split(' ')[1] == "8:01:00" && new Date().toLocaleString('zh-tw').split(' ')[2] == "AM"){
-    if (new Date().toLocaleString('zh-tw').split(' ')[1] == "8:01:00") {
+    if (new Date().toLocaleString('zh-tw').split(' ')[1] == "08:01:00") {
         let push = async () => {
             let serviceList = [];
             let db;
